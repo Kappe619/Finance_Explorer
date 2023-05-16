6f4ffe665cb25eff051ade6d2f6b024fedaffb7f
Britta <kappe619@gmail.com>
2023-05-16 15:45:42 +0200

    feat: kontostand_plot

--- src/finance_explorer.py
+++ src/finance_explorer.py
@@ -1,11 +1,37 @@
 import pandas as pd
 import tkinter as tk
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+
+
 
 def button_click():
-    # Hier kannst du den Code für die Aktion schreiben, die beim Klicken des Buttons ausgeführt werden soll
-    # Zum Beispiel: Eine Meldung anzeigen
-    label.config(text="Button wurde geklickt!")
+    show_kontostand_plot()
+    
+
+def show_kontostand_plot():
+    # Spalten, die behalten werden sollen
+    columns_to_keep = ["Buchungstag", "Saldo nach Buchung"]
+
+    # Lade die CSV-Datei mit Pandas und behalte nur die angegebenen Spalten
+    data = pd.read_csv("data/Transactions_2023-03.csv", delimiter=";", usecols=columns_to_keep)
+
+    # Konvertiere das Datumsformat in einen geeigneten Datentyp
+    data["Buchungstag"] = pd.to_datetime(data["Buchungstag"], format="%d.%m.%Y")
+
+    # Konvertiere den Kontostand in den korrekten Datentyp und ändere das Dezimaltrennzeichen
+    data["Saldo nach Buchung"] = data["Saldo nach Buchung"].str.replace(",", ".").astype(float)
+
+    # Erstelle den Plot des Kontostands
+    plt.plot(data["Buchungstag"], data["Saldo nach Buchung"])
+    plt.xlabel("Buchungstag")
+    plt.ylabel("Saldo nach Buchung")
+    plt.title("Kontostand über Zeit")
+    plt.xticks(rotation=45)
+    plt.show()
+
+
+
 
 
 root = tk.Tk()
@@ -18,6 +44,3 @@ button = tk.Button(root, text="Klick mich", command=button_click)  # command=but
 button.pack()  # Platziere den Button im Hauptfenster
 
 root.mainloop()
-
-
-#def button_click():
